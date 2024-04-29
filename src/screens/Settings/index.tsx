d08208cf591534513e2502af252511a754988d51
Kritajna Shrestha <kritajnshrest@gmaail.com>
2024-04-29 23:59:16 +0545

    fix: updated settings page

--- src/screens/Settings/index.tsx
+++ src/screens/Settings/index.tsx
@@ -1,26 +1,35 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
 import HeaderComponent from '../../components/HeaderComponent';
+import SettingsCard from '../../components/SettingsCard';
+import {CustomIcon} from '../../components/CustomIcon';
 
 const Settings = () => {
   return (
-    // <View>
-    //   <HeaderComponent title='Settings' />
-    //   <Text>This is settings</Text>
-    // </View>
+    <View>
+      <HeaderComponent title="Settings" />
+      <View style={{marginTop: 10}}>
+        <SettingsCard
+          title="Get Hints"
+          library1="Octicons"
+          name1="light-bulb"
+        />
 
-    <HeaderComponent title='Settings'>
-        <View>
-            <Text>This is settings</Text>
-        </View>
-    </HeaderComponent>
-  )
-}
+        <SettingsCard
+          title="History"
+          library1="MaterialCommunityIcons"
+          name1="history"
+        />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
+  container: {
+    flex: 1
+  },
+  
 });
 
-export default Settings
+export default Settings;
