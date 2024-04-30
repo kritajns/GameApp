df61250b6e55587d06dfad62012e881c0b681185
Kritajna Shrestha <kritajnshrest@gmail.com>
2024-04-30 23:55:55 +0545

    fix: update settings screen and add navigation

--- src/screens/Settings/index.tsx
+++ src/screens/Settings/index.tsx
@@ -4,11 +4,17 @@ import HeaderComponent from '../../components/HeaderComponent';
 import SettingsCard from '../../components/SettingsCard';
 import {CustomIcon} from '../../components/CustomIcon';
 
-const Settings = () => {
+interface SettingsProps {
+  navigation?: any;
+}
+
+const Settings: React.FC<SettingsProps> = ({navigation}) => {
   return (
     <View>
       <HeaderComponent title="Settings" />
       <View style={{marginTop: 10}}>
+        <SettingsCard title="Premium" library1="Octicons" name1="light-bulb" isIcon={false} />
+
         <SettingsCard
           title="Get Hints"
           library1="Octicons"
@@ -20,6 +26,64 @@ const Settings = () => {
           library1="MaterialCommunityIcons"
           name1="history"
         />
+
+        <SettingsCard
+          title="Statistics"
+          library1="MaterialCommunityIcons"
+          name1="history"
+        />
+
+        <SettingsCard
+          title="How to Play"
+          library1="MaterialCommunityIcons"
+          name1="history"
+        />
+
+        <SettingsCard
+          title="Strategies"
+          library1="MaterialCommunityIcons"
+          name1="history"
+          onPress={() => navigation?.navigate('Strategies')}
+        />
+
+        <SettingsCard
+          title="Feedback"
+          library1="MaterialCommunityIcons"
+          name1="history"
+        />
+
+        <SettingsCard title="Themes" library1="Octicons" name1="light-bulb" />
+
+        <SettingsCard
+          title="Sound Effects"
+          library1="MaterialCommunityIcons"
+          name1="history"
+        />
+
+<SettingsCard
+          title="Vibrate"
+          library1="MaterialCommunityIcons"
+          name1="history"
+        />
+
+<SettingsCard
+          title="Timer"
+          library1="MaterialCommunityIcons"
+          name1="history"
+        />
+
+        <SettingsCard
+          title="Redeem Code"
+          library1="Octicons"
+          name1="light-bulb"
+          isIcon={false}
+        />
+
+        <SettingsCard
+          title="Manage Subscription"
+          library1="Octicons"
+          name1="light-bulb"
+        />
       </View>
     </View>
   );
@@ -27,9 +91,8 @@ const Settings = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
-  
 });
 
 export default Settings;
